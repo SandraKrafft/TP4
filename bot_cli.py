071892d8ecaf169fb1f86a9418949c3fe96ca241
Krafft <skrafft@et.esiea.fr>
2022-04-19 13:39:18 +0200

    FICHIER DE LANCEMENT

--- bot_cli.py
+++ bot_cli.py
@@ -1,5 +1,8 @@
 from tp4_bot import MyBot
-from argparse import ArgumentParser
+from discord.ext import commands
+import discord
+from argparse import ArgumentParser, Namespace
+import json
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
@@ -7,3 +10,39 @@ def parse_args() -> Namespace:
         "-c", "--config", help="Config file", required=True, dest="config"
     )
     return parser.parse_args()
+
+def get_config(config_file: str)-> dict:
+
+    with open(config_file, "r") as f:
+
+        config = json.load(f)
+
+    return config
+
+
+
+def main(config : dict) -> bool:
+
+    token = config["TOKEN"]
+    prefix = config["prefix"]
+    intents = discord.Intents.default()
+    intents.presences = True
+    intents.members = True
+    mybot = MyBot( prefix,intents=intents)
+
+    mybot.run(token)
+
+    pass
+
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+
+
+    config = get_config(args.config)
+
+    main(config)
+
+    pass
