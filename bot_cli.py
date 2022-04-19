ec95f1809f8db03e5ec9f1e0f3c656c9ba010381
Krafft <skrafft@et.esiea.fr>
2022-04-19 12:39:52 +0200

    commit du fichier run

---
+++ bot_cli.py
@@ -0,0 +1,9 @@
+from tp4_bot import MyBot
+from argparse import ArgumentParser
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-c", "--config", help="Config file", required=True, dest="config"
+    )
+    return parser.parse_args()
